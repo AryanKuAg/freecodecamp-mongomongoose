ee7c40a0303c7ff7b4abd32bceff081c395926ba
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-01-16 01:25:32 +0530

    p changed

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ var mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+console.log(process.env.MONGO_URI)
+
 let Person;
 
 const createAndSavePerson = (done) => {
