712f181d0e9713d98d49de95610d4d4faaa050e0
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-01-17 23:06:08 +0530

    little bit cured

--- myApp.js
+++ myApp.js
@@ -1,9 +1,8 @@
-require('dotenv').config();
 var mongoose = require('mongoose');
+require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-console.log(process.env.MONGO_URI)
 
 let Person;
 
